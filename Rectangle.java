19f289443de15448ef1605de3c014acbb3c5a563
Yaryna Matselyukh <ym449@exeter.ac.uk>
2026-02-08 09:34:46 +0000

    completed part 2

--- Rectangle.java
+++ Rectangle.java
@@ -1,46 +1,90 @@
 // A Rectangle class
 public class Rectangle {
-  
-  // 4 instance attributes
-  public double width;
-  public double height;
-  public double originX = 0.0;
-  public double originY = 0.0;
 
-  // 1 static attributes
-  public static final int NUMBER_OF_SIDES = 4;
+    // 4 instance attributes
+    public double width;
+    public double height;
+    public double originX = 0.0;
+    public double originY = 0.0;
 
-  // main constructor
-  public Rectangle(double width, double height, double originX, double originY) {
-  this.width = width;
-  this.height = height;
-  this.originX = originX;
-  this.originY = originY;
-}
-  // second constructor:
-  public Rectangle(double width, double height) {
-    this(width, height, 0, 0);
-  }
+    // 1 static attribute
+    public static final int NUMBER_OF_SIDES = 4;
 
-  // second constructor:
-  public Rectangle() {
-    this(1, 1, 0, 0);
-  }
+    // main constructor
+    public Rectangle(double width, double height, double originX, double originY) {
+        this.width = width;
+        this.height = height;
+        this.originX = originX;
+        this.originY = originY;
+    }
 
-  // method: move the rectangle
-  public void move(double dx, double dy) {
-	   originX += dx;
-	   originY += dy;
-  }
+    // second constructor
+    public Rectangle(double width, double height) {
+        this(width, height, 0, 0);
+    }
 
-  // method: compute the area of the rectangle
-  public double getArea() {
-    return width * height;
-  }
-  
-  // method: compute the area of the rectangle
-  public double getPerimeter() {
-    return 2 * (width + height);
-  }
-  
+    // second constructor
+    public Rectangle() {
+        this(1, 1, 0, 0);
+    }
+
+    // method: move the rectangle
+    public void move(double dx, double dy) {
+        originX += dx;
+        originY += dy;
+    }
+
+    // scale width and height separately
+    public void scale(double scaleX, double scaleY) {
+        width = width * scaleX;
+        height = height * scaleY;
+    }
+
+    // scale width and height by the same factor
+    public void scale(double scaleFactor) {
+        scale(scaleFactor, scaleFactor);
+    }
+
+    // method: compute the area of the rectangle
+    public double getArea() {
+        return width * height;
+    }
+
+    // method: compute the perimeter of the rectangle
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    public boolean isOverlappedWith(Rectangle r) {
+
+        double thisLeft   = this.originX;
+        double thisRight  = this.originX + this.width;
+        double thisBottom = this.originY;
+        double thisTop    = this.originY + this.height;
+
+        double rLeft   = r.originX;
+        double rRight  = r.originX + r.width;
+        double rBottom = r.originY;
+        double rTop    = r.originY + r.height;
+
+        if (thisRight <= rLeft || thisLeft >= rRight ||
+            thisTop <= rBottom || thisBottom >= rTop) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean areOverlapping(Rectangle r1, Rectangle r2) {
+        return r1.isOverlappedWith(r2);
+    }
+
+    public double calcRatio() {
+        return width / height;
+    }
+
+    public boolean isSquare() {
+        double ratio = calcRatio();
+        return ratio >= 0.999 && ratio <= 1.001;
+    }
 }
--- RectangleApp.java
+++ RectangleApp.java
@@ -1,40 +1,53 @@
 // An application to create and manipulate rectangles
 public class RectangleApp {
-	//To be excutable, need a main method
-	public static void main( String[] args ) {
-		System.out.println("Creating myRect1");
-		
-		Rectangle myRect1; //myRect is not instantiated
-		myRect1 = new Rectangle(20.0, 8.0,30,30); //instantiated
-		
-		//static field
-		System.out.println("Rectangle has " + Rectangle.NUMBER_OF_SIDES + " sides");
-		//instance fields
-		System.out.println("Width: "+myRect1.width+", Height: "+myRect1.height);
-		System.out.println("Origin is: "+myRect1.originX+","+myRect1.originY);
-		//calling methods
-		System.out.println("Area: "+myRect1.getArea());
-		
-		System.out.println("\nMoving myRect1");
-		myRect1.move(0,10);//the object's state is changed
-		System.out.println("Origin is: "+myRect1.originX+","+myRect1.originY);
+    //To be excutable, need a main method
+    public static void main(String[] args) {
+        System.out.println("Creating myRect1");
+        
+        Rectangle myRect1; 
+        myRect1 = new Rectangle(20.0, 8.0, 30, 30); 
+        
+        //static field
+        System.out.println("Rectangle has " + Rectangle.NUMBER_OF_SIDES + " sides");
+        //instance fields
+        System.out.println("Width: " + myRect1.width + ", Height: " + myRect1.height);
+        System.out.println("Origin is: " + myRect1.originX + "," + myRect1.originY);
+        //calling methods
+        System.out.println("Area: " + myRect1.getArea());
+        
+        System.out.println("\nMoving myRect1");
+        myRect1.move(0, 10);
+        System.out.println("Origin is: " + myRect1.originX + "," + myRect1.originY);
 
-		System.out.println("Changing width of myRect1");
-		myRect1.width = 16;//the object's state is changed
-		System.out.println("Width: "+myRect1.width+", Height: "+myRect1.height);
+        System.out.println("Changing width of myRect1");
+        myRect1.width = 16;
+        System.out.println("Width: " + myRect1.width + ", Height: " + myRect1.height);
 
-		/*
-		System.out.println("Creating myRect2");
-		Rectangle myRect2 = new Rectangle(20.0, 8.0);
-		System.out.println("Width: "+myRect2.width+", Height: "+myRect2.height);
-		System.out.println("Origin: "+myRect2.originX+","+myRect2.originY);
-		 */
+        System.out.println("Creating myRect2");
+        Rectangle myRect2 = new Rectangle(20.0, 8.0);
+        System.out.println("Width: " + myRect2.width + ", Height: " + myRect2.height);
+        System.out.println("Origin: " + myRect2.originX + "," + myRect2.originY);
 
+        System.out.println("Creating myRect3");
+        Rectangle myRect3 = new Rectangle(); 
+        System.out.println("Width: " + myRect3.width + ", Height: " + myRect3.height);
+        System.out.println("Origin: " + myRect3.originX + "," + myRect3.originY);
 
-		System.out.println("Creating myRect3");
-		Rectangle myRect3 = new Rectangle(); 
-		System.out.println("Width: "+myRect3.width+", Height: "+myRect3.height);
-		System.out.println("Origin: "+myRect3.originX+","+myRect3.originY);
+        System.out.println("\n--- Ratio & square tests ---");
+        System.out.println("myRect3 ratio: " + myRect3.calcRatio());
+        System.out.println("myRect3 is square: " + myRect3.isSquare());
 
-	}
+        myRect1.scale(0.5);     
+        myRect2.scale(1, 3);     
+        myRect3.scale(15, 10);   
+
+        Rectangle myRect4 = new Rectangle(30.0, 5.0, 10, 10);
+        Rectangle myRect5 = new Rectangle(50.0, 20.0, 0, 0);
+        Rectangle myRect6 = new Rectangle(20.0, 40.0, 500, 500);
+
+        System.out.println("myRect4 overlaps myRect5: " + myRect4.isOverlappedWith(myRect5));
+        System.out.println("myRect5 overlaps myRect4: " + myRect5.isOverlappedWith(myRect4));
+        System.out.println("myRect4 overlaps myRect6: " + myRect4.isOverlappedWith(myRect6));
+        System.out.println("myRect6 overlaps myRect4: " + myRect6.isOverlappedWith(myRect4));
+    }
 }
